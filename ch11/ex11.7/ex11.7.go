b2cff04f366eebfab8d23f115053b3244cc31055
Tublanx <rlgus03453@naver.com>
2022-11-20 00:34:41 +0900

    중첩 for문과 break

---
+++ ch11/ex11.7/ex11.7.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := 1, 1
+	found := false
+	for ; a <= 10; a++ {
+		for b = 1; b <= 9; b++ {
+			if a*b == 45 {
+				found = true
+				break
+			}
+		}
+		if found {
+			break
+		}
+	}
+	fmt.Printf("%d * %d = %d\n", a, b, a*b)
+}
